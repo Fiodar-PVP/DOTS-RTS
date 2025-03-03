8ef540e8cd0f7017c439db25b5229f5bb26a8519
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-03-03 13:19:15 +0900

    Add UnitMoverSystem script
    Uses created MoveSpeed component to move the unit

---
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -0,0 +1,25 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+partial struct UnitMoverSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach(
+            (RefRW<LocalTransform> localTransform, RefRO<MoveSpeed> moveSpeed, RefRW<PhysicsVelocity> physicsVelocity)
+            in SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
+        {
+            float3 targetPosition = localTransform.ValueRO.Position + new float3(5f ,0f, 0f);
+            float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value; 
+            physicsVelocity.ValueRW.Angular = float3.zero;
+        }
+    }
+}
