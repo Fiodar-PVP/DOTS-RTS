3b15227924873d00fb3609d5dea00336cf933a3a
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-17 15:08:10 +0900

    Add Selected Component
    Contains a reference to the Entity for the selected visual

---
+++ Assets/Scripts/Authoring/SelectedAuthoring.cs
@@ -0,0 +1,27 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class SelectedAuthoring : MonoBehaviour
+{
+    [SerializeField] private GameObject visualGameObject;
+
+    public class Baker : Baker<SelectedAuthoring>
+    {
+        public override void Bake(SelectedAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Selected 
+            {
+                visualEntity = GetEntity(authoring.visualGameObject, TransformUsageFlags.Dynamic)
+            });
+            SetComponentEnabled<Selected>(entity, false);
+        }
+    }
+}
+
+public partial struct Selected : IComponentData, IEnableableComponent
+{
+    public Entity visualEntity;
+}
+
+
