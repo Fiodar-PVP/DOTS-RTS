3cb828176dfb13738a74957776ba384cf3e34ebf
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-20 18:32:35 +0900

    Add Faction to Unit Authoring

--- Assets/Scripts/Authoring/UnitAuthoring.cs
+++ Assets/Scripts/Authoring/UnitAuthoring.cs
@@ -3,18 +3,24 @@ using UnityEngine;
 
 public class UnitAuthoring : MonoBehaviour
 {
+    [SerializeField] private Faction faction;
+
     public class UnitAuthoringBaker : Baker<UnitAuthoring>
     {
         public override void Bake(UnitAuthoring authoring)
         {
             Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
-            AddComponent(entity, new Unit());
+            AddComponent(entity, new Unit 
+            {
+                faction = authoring.faction,
+            });
+            AddComponent(entity, authoring.faction.GetComponentType());
         }
     }
 }
 
 public struct Unit : IComponentData
 {
-
+    public Faction faction;
 }
 
