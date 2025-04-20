28f4e5c8a14389a2e6264eac1ae7671ba302ec54
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-20 19:07:44 +0900

    Create FindTarget Component

---
+++ Assets/Scripts/Authoring/FindTargetAuthoring.cs
@@ -0,0 +1,26 @@
+using System;
+using Unity.Entities;
+using UnityEngine;
+
+public class FindTargetAuthoring : MonoBehaviour
+{
+    [SerializeField] private FindTarget value;
+
+    public class Baker : Baker<FindTargetAuthoring>
+    {
+        public override void Bake(FindTargetAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, authoring.value);
+        }
+    }
+}
+
+[Serializable]
+public struct FindTarget : IComponentData
+{
+    public float range;
+    public Faction targetFaction;
+    public float timer;
+    public float timerMax;
+}
