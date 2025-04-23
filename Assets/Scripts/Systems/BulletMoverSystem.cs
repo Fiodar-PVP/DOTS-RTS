831ae284e9425de92827da0c9a82dabb32844b29
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-23 12:33:53 +0900

    Set up BulletMover System
    Move the bullet towards target, damages target on contact, destroys bullet.

---
+++ Assets/Scripts/Systems/BulletMoverSystem.cs
@@ -0,0 +1,48 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+partial struct BulletMoverSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        NativeList<Entity> entityList = new NativeList<Entity>(Allocator.Temp);
+
+        foreach((RefRW<LocalTransform> localTransform, RefRO<Bullet> bullet, RefRO<Target> target, Entity entity) 
+            in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Bullet>, RefRO<Target>>().WithEntityAccess())
+        {
+            RefRO<LocalTransform> targetTransform = SystemAPI.GetComponentRO<LocalTransform>(target.ValueRO.targetEntity);
+            float3 moveDirection = targetTransform.ValueRO.Position - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            float distanceBefore = math.distancesq(targetTransform.ValueRO.Position, localTransform.ValueRO.Position);
+
+            localTransform.ValueRW.Position += moveDirection * bullet.ValueRO.speed * SystemAPI.Time.DeltaTime;
+
+            float distanceAfter = math.distancesq(targetTransform.ValueRO.Position, localTransform.ValueRO.Position);
+
+            if(distanceAfter > distanceBefore)
+            {
+                //Overshoot the target
+                localTransform.ValueRW.Position = targetTransform.ValueRO.Position;
+            }
+
+            float destroyDistanceSq = 0.2f;
+            float currentDistanceToTargetSq = math.distancesq(targetTransform.ValueRO.Position, localTransform.ValueRO.Position);
+
+            if(currentDistanceToTargetSq <= destroyDistanceSq)
+            {
+                entityList.Add(entity);
+
+                RefRW<Health> health = SystemAPI.GetComponentRW<Health>(target.ValueRO.targetEntity);
+                health.ValueRW.healthAmount -= bullet.ValueRO.damageAmount;
+            }
+        }
+
+        EntityCommandBuffer entityCommandBuffer = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
+        entityCommandBuffer.DestroyEntity(entityList.AsArray());
+    }
+}
