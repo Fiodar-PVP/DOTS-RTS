e63a4e7fafa96064be0154d149faa15ec2580ec3
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-23 12:33:21 +0900

    Add Bullet Component

---
+++ Assets/Scripts/Authoring/BulletAuthoring.cs
@@ -0,0 +1,23 @@
+using System;
+using Unity.Entities;
+using UnityEngine;
+
+public class BulletAuthoring : MonoBehaviour
+{
+    [SerializeField] private Bullet value;
+    public class BulletAuthoringBaker : Baker<BulletAuthoring>
+    {
+        public override void Bake(BulletAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, authoring.value);
+        }
+    }
+}
+
+[Serializable]
+public struct Bullet : IComponentData
+{
+    public float speed;
+    public int damageAmount;
+}
