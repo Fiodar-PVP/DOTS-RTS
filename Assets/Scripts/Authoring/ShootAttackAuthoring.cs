89cb338da4cd17538807c419c759ab332b7ae083
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-24 18:47:42 +0900

    Store BulletSpawnPosition on Shoot Attack Component

--- Assets/Scripts/Authoring/ShootAttackAuthoring.cs
+++ Assets/Scripts/Authoring/ShootAttackAuthoring.cs
@@ -1,16 +1,26 @@
 using System;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ShootAttackAuthoring : MonoBehaviour
 {
-    [SerializeField] private ShootAttack value;
+    [SerializeField] private Transform bulletSpawnPositionTransform;
+    [SerializeField] private float attackDistance;
+    [SerializeField] private int damageAmount;
+    [SerializeField] private float timerMax;
     public class Baker : Baker<ShootAttackAuthoring>
     {
         public override void Bake(ShootAttackAuthoring authoring)
         {
             Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
-            AddComponent(entity, authoring.value);
+            AddComponent(entity, new ShootAttack
+            {
+                bulletSpawnLocalPosition = authoring.bulletSpawnPositionTransform.localPosition,
+                attackDistanceSq = authoring.attackDistance * authoring.attackDistance,
+                damageAmount = authoring.damageAmount,
+                timerMax = authoring.timerMax,
+            });
         }
     }
 }
@@ -18,6 +28,8 @@ public class ShootAttackAuthoring : MonoBehaviour
 [Serializable]
 public struct ShootAttack : IComponentData
 {
+    public float3 bulletSpawnLocalPosition;
+    public float attackDistanceSq;
     public int damageAmount;
     public float timer;
     public float timerMax;
