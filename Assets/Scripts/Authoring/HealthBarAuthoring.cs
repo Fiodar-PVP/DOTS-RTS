a0b337658604ebf9cb7b15a352b91faebee8691c
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-04-26 19:25:25 +0900

    Add HealthBar Component

---
+++ Assets/Scripts/Authoring/HealthBarAuthoring.cs
@@ -0,0 +1,27 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class HealthBarAuthoring : MonoBehaviour
+{
+    [SerializeField] private GameObject parentGameObject;
+    [SerializeField] private GameObject barVisualGameObject;
+
+    public class HealthBarAuthoringBaker : Baker<HealthBarAuthoring>
+    {
+        public override void Bake(HealthBarAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, new HealthBar
+            {
+                parentEntity = GetEntity(authoring.parentGameObject, TransformUsageFlags.Dynamic),
+                barVisualEntity = GetEntity(authoring.barVisualGameObject, TransformUsageFlags.NonUniformScale),
+            });
+        }
+    }
+}
+
+public struct HealthBar : IComponentData
+{
+    public Entity parentEntity;
+    public Entity barVisualEntity;
+}
