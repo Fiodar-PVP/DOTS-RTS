8af5e31189816bab04dfe4e721c6d8cb70db8452
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-05 21:03:30 +0900

    Store next AnimationType on ActiveAnimation

--- Assets/Scripts/Authoring/ActiveAnimationAuthoring.cs
+++ Assets/Scripts/Authoring/ActiveAnimationAuthoring.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class ActiveAnimationAuthoring : MonoBehaviour
 {
+    [SerializeField] private AnimationType nextAnimationType;
     public class Baker : Baker<ActiveAnimationAuthoring>
     {
         public override void Bake(ActiveAnimationAuthoring authoring)
         {
             Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
-            AddComponent(entity, new ActiveAnimation());
+            AddComponent(entity, new ActiveAnimation 
+            {
+                nextAnimationType = authoring.nextAnimationType,
+            });
         }
     }
 }
@@ -18,4 +22,5 @@ public struct ActiveAnimation : IComponentData
     public int frame;
     public float frameTimer;
     public AnimationType activeAnimationType;
+    public AnimationType nextAnimationType;
 }
