9bf3d00da78271fe43392dd5cf1c5ae102a93a04
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-05 22:12:30 +0900

    Reset Melee Attack and Shoot Animation after 1 replay

--- Assets/Scripts/Systems/ActiveAnimationSystem.cs
+++ Assets/Scripts/Systems/ActiveAnimationSystem.cs
@@ -41,6 +41,16 @@ partial struct ActiveAnimationSystem : ISystem
                 activeAnimation.ValueRW.frameTimer -= animationData.frameTimerMax;
                 activeAnimation.ValueRW.frame = (activeAnimation.ValueRO.frame + 1) % animationData.frameMax;
                 materialMeshInfo.ValueRW.MeshID = animationData.batchMeshArray[activeAnimation.ValueRW.frame];
+
+                if(activeAnimation.ValueRO.frame == 0 && activeAnimation.ValueRO.activeAnimationType == AnimationType.SoldierShoot)
+                {
+                    activeAnimation.ValueRW.activeAnimationType = AnimationType.None;
+                }
+
+                if (activeAnimation.ValueRO.frame == 0 && activeAnimation.ValueRO.activeAnimationType == AnimationType.ZombieAttack)
+                {
+                    activeAnimation.ValueRW.activeAnimationType = AnimationType.None;
+                }
             }
         }
     }
