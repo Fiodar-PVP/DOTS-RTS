827653ddb69c3776466700b8be8d74b012fd9cf8
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-10 22:20:31 +0900

    Update UnitDataSO and respective SO
    Store information regarding how much time required to spawn particular unit type. Add functionality to get Entity Prefab which is respective to unit type in UnitDataSO.

--- Assets/Scripts/UnitDataSO.cs
+++ Assets/Scripts/UnitDataSO.cs
@@ -1,7 +1,21 @@
+using Unity.Entities;
 using UnityEngine;
 
 [CreateAssetMenu()]
 public class UnitDataSO : ScriptableObject
 {
     public UnitType unitType;
+    public float progressMax;
+
+    public Entity GetPrefabEntity(EntitiesReferences entitiesReferences)
+    {
+        switch(unitType)
+        {
+            default:
+            case UnitType.None:
+            case UnitType.Soldier: return entitiesReferences.soldierPrefabEntity;
+            case UnitType.Scout: return entitiesReferences.scoutPrefabEntity;
+            case UnitType.Zombie: return entitiesReferences.zombiePrefabEntity;
+        }
+    }
 }
