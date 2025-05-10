6de4aa7291888d637e3c87c656ffdf2896a790c7
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-10 22:17:46 +0900

    Update EntitiesReferences Component with Scout and Soldier entity prefabs

--- Assets/Scripts/Authoring/EntitiesReferencesAuthoring.cs
+++ Assets/Scripts/Authoring/EntitiesReferencesAuthoring.cs
@@ -6,6 +6,8 @@ public class EntitiesReferencesAuthoring : MonoBehaviour
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private GameObject zombiePrefab;
     [SerializeField] private GameObject shootLightPrefab;
+    [SerializeField] private GameObject soldierPrefab;
+    [SerializeField] private GameObject scoutPrefab;
 
     public class Baker : Baker<EntitiesReferencesAuthoring>
     {
@@ -17,6 +19,8 @@ public class EntitiesReferencesAuthoring : MonoBehaviour
                 bulletPrefabEntity = GetEntity(authoring.bulletPrefab, TransformUsageFlags.Dynamic),
                 zombiePrefabEntity = GetEntity(authoring.zombiePrefab, TransformUsageFlags.Dynamic),
                 shootLightPrefabEntity = GetEntity(authoring.shootLightPrefab, TransformUsageFlags.Dynamic),
+                soldierPrefabEntity = GetEntity(authoring.soldierPrefab, TransformUsageFlags.Dynamic),
+                scoutPrefabEntity = GetEntity(authoring.scoutPrefab, TransformUsageFlags.Dynamic),
             });
         }
     }
@@ -27,4 +31,6 @@ public struct EntitiesReferences : IComponentData
     public Entity bulletPrefabEntity;
     public Entity zombiePrefabEntity;
     public Entity shootLightPrefabEntity;
+    public Entity soldierPrefabEntity;
+    public Entity scoutPrefabEntity;
 }
