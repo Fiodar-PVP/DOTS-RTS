564cbc9d07c1c242d9d9b5f52649968fdced536a
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-11 13:08:06 +0900

    Set up onUnitEnqueueChanged Event for BuildingBarrack
    We check which BuildingBarrack entities fired onUnitEnqueueChanged custom event inside DOTS, record them in list, then pass them to GameObject DOTSEventManager which fire normal c# events which will be used inside BuildingBarrackUI

--- Assets/Scripts/Systems/ResetEventSystem.cs
+++ Assets/Scripts/Systems/ResetEventSystem.cs
@@ -14,13 +14,20 @@ partial struct ResetEventSystem : ISystem
         jobHandleNativeArray = new NativeArray<JobHandle>(4, Allocator.Persistent);
     }
 
-    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         jobHandleNativeArray[0] = new ResetSeletedEventJob().ScheduleParallel(state.Dependency);
         jobHandleNativeArray[1] = new ResetHealthEventJob().ScheduleParallel(state.Dependency);
         jobHandleNativeArray[2] = new ResetShootAttackEventJob().ScheduleParallel(state.Dependency);
         jobHandleNativeArray[3] = new ResetMeleeAttackEventJob().ScheduleParallel(state.Dependency);
+
+        NativeList<Entity> onBuildingBarrackUnitEnqueueChangedEntityList = new NativeList<Entity>(Allocator.TempJob);
+        new ResetBuildingBarrackEventJob
+        {
+            onUnitQueueChangedEntityList = onBuildingBarrackUnitEnqueueChangedEntityList.AsParallelWriter()
+        }.ScheduleParallel(state.Dependency).Complete();
+
+        DOTSEventManager.Instance.TriggerOnBuildinBarrackUnitQueueChanged(onBuildingBarrackUnitEnqueueChangedEntityList);
 
         state.Dependency = JobHandle.CombineDependencies(jobHandleNativeArray);
     }
@@ -61,5 +68,20 @@ public partial struct ResetMeleeAttackEventJob : IJobEntity
     public void Execute(ref MeleeAttack meleeAttack)
     {
         meleeAttack.onAttack = false;
+    }
+}
+
+[BurstCompile]
+public partial struct ResetBuildingBarrackEventJob : IJobEntity
+{
+    public NativeList<Entity>.ParallelWriter onUnitQueueChangedEntityList;
+    public void Execute(ref BuildingBarrack buildingBarrack, Entity entity)
+    {
+        if (buildingBarrack.onUnitQueueChanged)
+        {
+            onUnitQueueChangedEntityList.AddNoResize(entity);
+        }
+
+        buildingBarrack.onUnitQueueChanged = false;
     }
 }
