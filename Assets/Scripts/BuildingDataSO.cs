c671af6293a8019449ddc443a7c92fdb389a3629
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-12 16:27:42 +0900

    Store Prefab and buildingDistanceMin on BuildingDataSO

--- Assets/Scripts/BuildingDataSO.cs
+++ Assets/Scripts/BuildingDataSO.cs
@@ -1,7 +1,22 @@
+using Unity.Entities;
 using UnityEngine;
 
 [CreateAssetMenu()]
 public class BuildingDataSO : ScriptableObject
 {
     public BuildingType buildingType;
+    public Transform prefab;
+    public float buildingDistanceMin;
+
+    public Entity GetEntityPrefab(EntitiesReferences entitiesReferences)
+    {
+        switch (buildingType)
+        {
+            default:
+            case BuildingType.None:
+            case BuildingType.ZombieSpawner:
+            case BuildingType.Barrack : return entitiesReferences.barrackPrefabEntity;
+            case BuildingType.Tower : return entitiesReferences.towerPrefabEntity;
+        }
+    }
 }
