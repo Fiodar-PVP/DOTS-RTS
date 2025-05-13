7644269a595ae55eed9d01a163d530b90dfc02e7
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-13 10:52:08 +0900

    Create BuildPlacementManagerUI

---
+++ Assets/Scripts/UI/BuildingPlacementManagerUI.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacementManagerUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform buttonContainer;
+    [SerializeField] private RectTransform buttonTemplate;
+    [SerializeField] private BuildingDataListSO buildingDataListSO;
+
+    private Dictionary<BuildingDataSO, BuildingPlacementManagerUI_SingleButton> buttonDictionary;
+
+    private void Awake()
+    {
+        buttonTemplate.gameObject.SetActive(false);
+        buttonDictionary = new Dictionary<BuildingDataSO, BuildingPlacementManagerUI_SingleButton>();
+
+        foreach(BuildingDataSO buildingDataSO in buildingDataListSO.buildingDataSOList)
+        {
+            if (!buildingDataSO.shouldShowInBuildingPlacementManager)
+            {
+                continue;
+            }
+
+            Transform buttonTemplateTransform = Instantiate(buttonTemplate, buttonContainer);
+            buttonTemplateTransform.gameObject.SetActive(true);
+
+            BuildingPlacementManagerUI_SingleButton buildingPlacementManagerUI_SingleButton
+                = buttonTemplateTransform.GetComponent<BuildingPlacementManagerUI_SingleButton>();
+            buttonDictionary[buildingDataSO] = buildingPlacementManagerUI_SingleButton;
+            buildingPlacementManagerUI_SingleButton.Setup(buildingDataSO);
+        }
+    }
+
+    private void Start()
+    {
+        BuildingPlacementManager.Instance.OnActiveBuildingDataSOChanged += BuildingPlacementManager_OnActiveBuildingDataSOChanged;
+
+        UpdateSelectedVisual();
+    }
+
+    private void BuildingPlacementManager_OnActiveBuildingDataSOChanged(object sender, System.EventArgs e)
+    {
+        UpdateSelectedVisual();
+    }
+
+    private void UpdateSelectedVisual()
+    {
+        foreach(BuildingDataSO buildingDataSO in buttonDictionary.Keys)
+        {
+            buttonDictionary[buildingDataSO].HideVisual();
+        }
+
+        BuildingDataSO activeBuildingDataSO = BuildingPlacementManager.Instance.GetActiveBuildingDataSO();
+        buttonDictionary[activeBuildingDataSO].ShowVisual();
+    }
+}
