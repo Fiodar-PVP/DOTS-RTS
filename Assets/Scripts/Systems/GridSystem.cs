1d2ff54fc17e4e5827dc58ab750d57d1f3b0fe1c
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-20 17:50:20 +0900

    Implement Flow Field Pathfinding Algorithm

--- Assets/Scripts/Systems/GridSystem.cs
+++ Assets/Scripts/Systems/GridSystem.cs
@@ -23,9 +23,12 @@ public partial struct GridSystem : ISystem
 
     public struct GridNode : IComponentData
     {
+        public int index;
         public int x;
         public int y;
-        public byte data;
+        public byte cost;
+        public byte bestCost;
+        public float2 vector;
     }
 
 #if(!GridDebug)
@@ -55,6 +58,7 @@ public partial struct GridSystem : ISystem
                 int index = CalculateIndex(x, y, width);
                 GridNode gridNode = new GridNode
                 {
+                    index = index,
                     x = x,
                     y = y,
                 };
@@ -83,6 +87,71 @@ public partial struct GridSystem : ISystem
     {
         GridSystemData gridSystemData = state.EntityManager.GetComponentData<GridSystemData>(state.SystemHandle);
 
+        NativeArray<RefRW<GridNode>> gridNodeArray = new NativeArray<RefRW<GridNode>>(gridSystemData.width * gridSystemData.height, Allocator.Temp);
+        int2 targetGridNodePosition = new int2(1,2);
+
+        for (int x = 0; x < gridSystemData.width; x++)
+        {
+            for(int y = 0; y< gridSystemData.height; y++)
+            {
+                int index = CalculateIndex(x, y, gridSystemData.width);
+                Entity gridNodeEntity = gridSystemData.gridMap.gridEntityArray[index];
+                RefRW<GridNode> gridNode = SystemAPI.GetComponentRW<GridNode>(gridNodeEntity);
+                gridNodeArray[index] = gridNode;
+
+                gridNode.ValueRW.vector = new float2(0, 1);
+                if(gridNode.ValueRW.x == targetGridNodePosition.x && gridNode.ValueRW.y == targetGridNodePosition.y)
+                {
+                    gridNode.ValueRW.cost = 0;
+                    gridNode.ValueRW.bestCost = 0;
+                }
+                else
+                {
+                    gridNode.ValueRW.cost = 1;
+                    gridNode.ValueRW.bestCost = byte.MaxValue;
+                }
+            }
+        }
+
+        NativeQueue<RefRW<GridNode>> gridNodeOpenQueue = new NativeQueue<RefRW<GridNode>>(Allocator.Temp);
+        int targetGridNodeIndex = CalculateIndex(targetGridNodePosition, gridSystemData.width);
+        RefRW <GridNode> targetGridNode = gridNodeArray[targetGridNodeIndex];
+        gridNodeOpenQueue.Enqueue(targetGridNode);
+
+        int safetyCheck = 1000;
+        while(gridNodeOpenQueue.Count > 0)
+        {
+            safetyCheck--;
+            if(safetyCheck < 0)
+            {
+                Debug.LogError("Too many iterations in Flow Field algorithm!");
+                break;
+            }
+
+            RefRW<GridNode> currentGridNode = gridNodeOpenQueue.Dequeue();
+
+            NativeList<RefRW<GridNode>> neighbourGridNodeList = GetNeighbourGridNodeList(currentGridNode, gridSystemData.width, gridSystemData.height, gridNodeArray);
+
+            foreach(RefRW<GridNode> neighbourGridNode in neighbourGridNodeList)
+            {
+                byte newBestCost = (byte)(currentGridNode.ValueRO.bestCost + neighbourGridNode.ValueRO.cost);
+                if (newBestCost < neighbourGridNode.ValueRO.bestCost)
+                {
+                    neighbourGridNode.ValueRW.bestCost = newBestCost;
+                    neighbourGridNode.ValueRW.vector = CalculateVector(
+                        neighbourGridNode.ValueRO.x, neighbourGridNode.ValueRO.y,
+                        currentGridNode.ValueRO.x, currentGridNode.ValueRO.y);
+
+                    gridNodeOpenQueue.Enqueue(neighbourGridNode);
+                }
+            }
+
+            neighbourGridNodeList.Dispose();
+        }
+
+        gridNodeOpenQueue.Dispose();
+        gridNodeArray.Dispose();
+        
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
@@ -93,7 +162,7 @@ public partial struct GridSystem : ISystem
                 int index = CalculateIndex(gridPosition.x, gridPosition.y, gridSystemData.width);
                 Entity gridNodeEntity = gridSystemData.gridMap.gridEntityArray[index];
                 RefRW<GridNode> gridNode = SystemAPI.GetComponentRW<GridNode>(gridNodeEntity);
-                gridNode.ValueRW.data = 1;
+                Debug.Log(gridNode.ValueRO.vector);
             }
         }
 
@@ -108,6 +177,69 @@ public partial struct GridSystem : ISystem
     {
         RefRW<GridSystemData> gridSystemData = state.EntityManager.GetComponentDataRW<GridSystemData>(state.SystemHandle);
         gridSystemData.ValueRW.gridMap.gridEntityArray.Dispose();
+    }
+
+    public static float2 CalculateVector(int fromX, int fromY, int toX, int toY)
+    {
+        return new float2(toX, toY) - new float2(fromX, fromY);
+    }
+
+    public static NativeList<RefRW<GridNode>> GetNeighbourGridNodeList(RefRW<GridNode> currentGridNode, int width, int height, NativeArray<RefRW<GridNode>> gridNodeArray)
+    {
+        NativeList<RefRW<GridNode>> neighbourGridNodeList = new NativeList<RefRW<GridNode>>(Allocator.Temp);
+        int nodeX = currentGridNode.ValueRO.x;
+        int nodeY = currentGridNode.ValueRO.y;
+
+        int2 positionLeft  = new int2(nodeX - 1, nodeY + 0);
+        int2 positionRight = new int2(nodeX + 1, nodeY + 0);
+        int2 positionUp    = new int2(nodeX + 0, nodeY + 1);
+        int2 positionDown  = new int2(nodeX + 0, nodeY - 1);
+
+        int2 positionLowerLeft  = new int2(nodeX - 1, nodeY - 1);
+        int2 positionUpperLeft  = new int2(nodeX - 1, nodeY + 1);
+        int2 positionLowerRight = new int2(nodeX + 1, nodeY - 1);
+        int2 positionUpperRight = new int2(nodeX + 1, nodeY + 1);
+
+        if(IsValidGridPosition(positionLeft, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionLeft, width)]);
+        }
+        if (IsValidGridPosition(positionRight, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionRight, width)]);
+        }
+        if (IsValidGridPosition(positionUp, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionUp, width)]);
+        }
+        if (IsValidGridPosition(positionDown, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionDown, width)]);
+        }
+
+        if (IsValidGridPosition(positionLowerLeft, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionLowerLeft, width)]);
+        }
+        if (IsValidGridPosition(positionUpperLeft, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionUpperLeft, width)]);
+        }
+        if (IsValidGridPosition(positionLowerRight, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionLowerRight, width)]);
+        }
+        if (IsValidGridPosition(positionUpperRight, width, height))
+        {
+            neighbourGridNodeList.Add(gridNodeArray[CalculateIndex(positionUpperRight, width)]);
+        }
+
+        return neighbourGridNodeList;
+    }
+
+    public static int CalculateIndex(int2 gridPosition, int width)
+    {
+        return CalculateIndex(gridPosition.x, gridPosition.y, width);
     }
 
     public static int CalculateIndex(int x, int y, int width)
