69a2191cecd3b92a4c80f76d0cfd09e230c422cb
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-05-24 20:45:20 +0900

    Update UnitMover System
    Move unit based on the movement direction (vectors) stored on the grid node it's currently on.

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -9,8 +9,55 @@ partial struct UnitMoverSystem : ISystem
     public const float REACHED_TARGET_STOP_DISTANCE_SQ = 2f;
 
     [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<GridSystem.GridSystemData>();
+    }
+
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        GridSystem.GridSystemData gridSystemData = SystemAPI.GetSingleton<GridSystem.GridSystemData>();
+        
+        foreach((
+            RefRW<UnitMover> unitMover, 
+            RefRO<LocalTransform> localTransform, 
+            RefRW<FlowFieldFollower> flowFieldFollower,
+            EnabledRefRW<FlowFieldFollower> flowFieldFollowerEnabled) 
+            in SystemAPI.Query<
+                RefRW<UnitMover>, 
+                RefRO<LocalTransform>, 
+                RefRW<FlowFieldFollower>,
+                EnabledRefRW<FlowFieldFollower>>())
+        {
+            int2 gridPosition = GridSystem.GetGridPosition(localTransform.ValueRO.Position, gridSystemData.gridNodeSize);
+            int index = GridSystem.CalculateIndex(gridPosition, gridSystemData.width);
+            Entity gridNodeEntity = gridSystemData.gridMap.gridEntityArray[index];
+            GridSystem.GridNode gridNode = SystemAPI.GetComponent<GridSystem.GridNode>(gridNodeEntity);
+
+            float3 targetMovementVector = GridSystem.GetMovementVector(gridNode.vector);
+
+            if(gridNode.cost == GridSystem.WALL_COST)
+            {
+                //Stepped on Wall Grid Node
+                targetMovementVector = flowFieldFollower.ValueRO.lastMovementVector;
+            }
+            else
+            {
+                flowFieldFollower.ValueRW.lastMovementVector = targetMovementVector;
+            }
+
+            unitMover.ValueRW.targetPosition =
+                GridSystem.GetWorldCenterPosition(gridPosition.x, gridPosition.y, gridSystemData.gridNodeSize) +
+                targetMovementVector * gridSystemData.gridNodeSize * 2f;
+
+            if(math.distance(localTransform.ValueRO.Position, flowFieldFollower.ValueRO.targetPosition) < gridSystemData.gridNodeSize)
+            {
+                unitMover.ValueRW.targetPosition = localTransform.ValueRO.Position;
+                flowFieldFollowerEnabled.ValueRW = false;
+            }
+        }
+
         UnitMoverJob unitMoverJob = new UnitMoverJob
         {
             deltaTime = SystemAPI.Time.DeltaTime
