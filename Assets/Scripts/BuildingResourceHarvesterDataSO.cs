8ba6440f3ad9045157456e5649ef0b07cea7b996
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-06-04 14:37:50 +0900

    Add BuildingResourceHarvesterDataSO
    For each resource type we have a dedicated resource harvester building. To store additional information, create BuildingResourceHarvesterDataSO which inherits from BuildingDataSO. Create SO for each resource harvester building

---
+++ Assets/Scripts/BuildingResourceHarvesterDataSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class BuildingResourceHarvesterDataSO : BuildingDataSO
+{
+    public ResourceType harvestableResourceType;
+    public float harvestDistanceMin;
+}
