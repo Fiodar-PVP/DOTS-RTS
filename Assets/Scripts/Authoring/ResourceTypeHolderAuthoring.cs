5b3dbb75ea86203b986770d09d86f21077bd7d4d
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-06-04 14:31:10 +0900

    Add ResourceTypeHolder Component
    This component is attached to each resource node

---
+++ Assets/Scripts/Authoring/ResourceTypeHolderAuthoring.cs
@@ -0,0 +1,24 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class ResourceTypeHolderAuthoring : MonoBehaviour
+{
+    [SerializeField] private ResourceType resourceType;
+
+    public class Baker : Baker<ResourceTypeHolderAuthoring>
+    {
+        public override void Bake(ResourceTypeHolderAuthoring authoring)
+        {
+            Entity entity = GetEntity(authoring, TransformUsageFlags.Dynamic);
+            AddComponent(entity, new ResourceTypeHolder
+            {
+                resourceType = authoring.resourceType
+            });
+        }
+    }
+}
+
+public struct ResourceTypeHolder : IComponentData
+{
+    public ResourceType resourceType;
+}
