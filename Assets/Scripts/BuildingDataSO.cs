187b6daeb8b1cfcff8a788047f24f6ff03bb9879
Fiodaru <f.perakhozhau@pvp.co.jp>
2025-06-09 11:35:52 +0900

    Update BuildingDataSO with Construction Data

--- Assets/Scripts/BuildingDataSO.cs
+++ Assets/Scripts/BuildingDataSO.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BuildingDataSO : ScriptableObject
 {
     public BuildingType buildingType;
+    public float constructionTimerMax;
+    public float constructionYOffset;
     public Transform prefab;
     public float buildingDistanceMin;
     public bool shouldShowInBuildingPlacementManager;
@@ -24,6 +26,21 @@ public class BuildingDataSO : ScriptableObject
             case BuildingType.IronHarvester : return entitiesReferences.ironHarvesterPrefabEntity;
             case BuildingType.GoldHarvester : return entitiesReferences.goldHarvesterPrefabEntity;
             case BuildingType.OilHarvester : return entitiesReferences.oilHarvesterPrefabEntity;
+        }
+    }
+
+    public Entity GetEntityVisualPrefab(EntitiesReferences entitiesReferences)
+    {
+        switch (buildingType)
+        {
+            default:
+            case BuildingType.None:
+            case BuildingType.ZombieSpawner:
+            case BuildingType.Barrack: return entitiesReferences.barrackVisualPrefabEntity;
+            case BuildingType.Tower: return entitiesReferences.towerVisualPrefabEntity;
+            case BuildingType.IronHarvester: return entitiesReferences.ironHarvesterVisualPrefabEntity;
+            case BuildingType.GoldHarvester: return entitiesReferences.goldHarvesterVisualPrefabEntity;
+            case BuildingType.OilHarvester: return entitiesReferences.oilHarvesterVisualPrefabEntity;
         }
     }
 
